b550adf488b5a128a87502ca74b62cc3023d84a1
L0CKnL0aD7 <daiderd@hotmail.com>
2011-08-18 18:56:07 +0200

    added dbProtocol

---
+++ SequelLogin/Protocols/DatabaseProtocol.h
@@ -0,0 +1,18 @@
+//
+//  DatabaseProtocol.h
+//  SequelLogin
+//
+//  Created by LnL on 18/08/11.
+//  Copyright 2011 L0CKnL0aD7. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@protocol DatabaseProtocol <NSObject>
+
+#pragma mark Properties
+@property (copy) id db;
+
+
+@end
